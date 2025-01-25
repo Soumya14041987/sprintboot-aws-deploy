8aa752e02856aa278322e4894d30dd44a1330ff9
Soumya14041987 <soumya14041987@gmail.com>
2025-01-25 11:32:39 +0000

    app code

---
+++ sprintbootapp-aws-deploy/sprintbootapp-aws-deploy/src/main/java/com/example/sprintbootapp_aws_deploy/SprintbootappAwsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootapp_aws_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootappAwsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootappAwsDeployApplication.class, args);
+	}
+
+}
---
+++ sprintbootapp-aws-deploy/sprintbootapp-aws-deploy/src/test/java/com/example/sprintbootapp_aws_deploy/SprintbootappAwsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootapp_aws_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootappAwsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
